672b0f8bd0e69ca5277c7f5b468536c6c3607ce2
Oliver Fei <Oliver Fei>
2017-05-07 16:59:13 -0400

    Collapse sidebar for smaller screens

--- scripts/downloads.js
+++ scripts/downloads.js
@@ -1,6 +1,22 @@
+var SHOW_LINKS_MESSAGE = "Show downloads/links";
+var HIDE_LINKS_MESSAGE = "Hide downloads/links";
+
 $(function(){
-    $(".collapse-link").on("click", function(){
-        var collapse_id = $(this).data("collapse-id");
-        $("#" + collapse_id).collapse("toggle");
+    $(".collapse-link").on("hide.bs.collapse", function(){
+        console.log('test');
+        $(this).text(SHOW_LINKS_MESSAGE);
+    });
+
+    $(".collapse-link").on("show.bs.collapse", function(){
+        $(this).text(HIDE_LINKS_MESSAGE);
+    });
+
+    $("#sidebar_toggle").on("click", function() {
+        if ($("#sidebar_container").position().left == 0){
+            $("#sidebar_container").css({left: "-250px"});
+        } else {
+            $("#sidebar_container").css({left: 0});
+        }
+        
     });
 });
